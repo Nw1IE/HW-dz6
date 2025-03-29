902b6e387eba9aa1545daa359162e371224ba251
Admin <Admin@DESKTOP-E7OIGPE>
2025-03-29 18:56:46 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Money.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Money
+    {
+        public double Sum { get; set; }
+
+
+
+        public static implicit operator Money(double sum)
+        {
+            return new Money(sum);
+        }
+
+
+        public static Money operator +(Money num1, Money num2)
+        {
+            return new Money(num1.Sum + num2.Sum);
+        }
+        public static Money operator +(Money num1, double num2)
+        {
+            Money res = num1 + num2;
+            return res;
+        }
+
+        public static Money operator -(Money num1, Money num2)
+        {
+            return new Money(num1.Sum - num2.Sum);
+        }
+
+        public static Money operator -(Money num1, double num2)
+        {
+            Money res = num1 - num2;
+            return res;
+        }
+
+
+
+        public static Money operator *(Money num1, int num2)
+        {
+            return new Money(num1.Sum * num2);
+        }
+
+        public static Money operator /(Money num1, int num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль запрещено!!");
+            }
+            return new Money(num1.Sum / num2);
+        }
+
+
+
+        public static Money operator ++(Money num)
+        {
+            return new Money(num.Sum + 0.1);
+        }
+
+        public static Money operator --(Money num)
+        {
+            return new Money(num.Sum - 0.1);
+        }
+
+
+
+        public static bool operator >(Money num1, Money num2)
+        {
+            return num1.Sum > num2.Sum;
+        }
+
+        public static bool operator <(Money num1, Money num2)
+        {
+            return num1.Sum < num2.Sum;
+        }
+
+        public static bool operator ==(Money num1, Money num2)
+        {
+            return num1.Sum == num2.Sum;
+        }
+
+        public static bool operator !=(Money num1, Money num2)
+        {
+            return num1.Sum != num2.Sum;
+        }
+
+        public Money(double sum)
+        {
+            Sum = sum;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using ConsoleApp4;
+
+namespace ConsoleApp4
+{
+    public class BankruptcyException : Exception
+    {
+        public BankruptcyException(string message) : base(message) { }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money num1 = 100;
+            Money num2 = 200;
+
+
+            try
+            {
+                CheckResult(num1 + num2, "+");
+                CheckResult(num2 - num1, "-");
+                CheckResult(num1 * 2, "*");
+                CheckResult(num1 / 2, "/");
+                Console.WriteLine($"==: {num1 == num2}");
+                Console.WriteLine($"!=: {num1 != num2}");
+                Console.WriteLine($">: {num1 > num2}");
+                Console.WriteLine($"<: {num1 < num2}");
+                CheckResult(num1++, "num1++");
+                CheckResult(num2++, "num2++");
+                CheckResult(num1--, "num1--");
+                CheckResult(num2--, "num2--");
+            }
+            catch (BankruptcyException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+        static void CheckResult(Money result, string operation)
+        {
+            if (result.Sum < 0)
+            {
+                throw new BankruptcyException($"Банкрот!! Результат операции {operation} меньше 0: {result.Sum}");
+            }
+            Console.WriteLine($"{operation}: {result.Sum}");
+        }
+    }
+}
